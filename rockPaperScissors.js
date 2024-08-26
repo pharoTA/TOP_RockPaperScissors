ec9c039248047f5fc3ad49b21dc795040b4cf038
Pierre Haro <pierre.haroland@gmail.com>
2024-08-26 18:09:31 +0200

    Add buttons to get the human player's move

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,4 @@
-
+/// Useful functions 
 
 function getComputerChoice() {
     const RPSIssues = ["rock", "paper", "scissors"];
@@ -16,52 +16,68 @@ function getHumanChoice() {
     }
 };
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            console.log("You won ! Rock vs scissors !");
+            humanScore += 1;
+        } else if (computerChoice == "paper") {
+            console.log("You lost ! Rock vs paper !");
+            computerScore += 1;
+        } else {
+            console.log("Rock vs rock - it's a draw !");
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You won ! Paper vs rock !");
+            humanScore += 1;
+        } else if (computerChoice == "scissors") {
+            console.log("You lost ! Paper vs scissors !");
+            computerScore += 1;
+        } else {
+            console.log("Paper vs paper - it's a draw !");
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+            console.log("You won ! Scissors vs paper !");
+            humanScore += 1;
+        } else if (computerChoice == "rock") {
+            console.log("You lost ! Scissors vs rock !");
+            computerScore += 1;
+        } else {
+            console.log("Scissors vs scissors - it's a draw !");
+        }
+    } else {
+        console.log("You played an unvalid hand");
+    };
+    console.log(`You ${humanScore} - Computer ${computerScore}`);
+};
 
+/*
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock") {
-            if (computerChoice == "scissors") {
-                console.log("You won ! Rock vs scissors !");
-                humanScore += 1;
-            } else if (computerChoice == "paper") {
-                console.log("You lost ! Rock vs paper !");
-                computerScore += 1;
-            } else {
-                console.log("Rock vs rock - it's a draw !");
-            }
-        } else if (humanChoice == "paper") {
-            if (computerChoice == "rock") {
-                console.log("You won ! Paper vs rock !");
-                humanScore += 1;
-            } else if (computerChoice == "scissors") {
-                console.log("You lost ! Paper vs scissors !");
-                computerScore += 1;
-            } else {
-                console.log("Paper vs paper - it's a draw !");
-            }
-        } else if (humanChoice == "scissors") {
-            if (computerChoice == "paper") {
-                console.log("You won ! Scissors vs paper !");
-                humanScore += 1;
-            } else if (computerChoice == "rock") {
-                console.log("You lost ! Scissors vs rock !");
-                computerScore += 1;
-            } else {
-                console.log("Scissors vs scissors - it's a draw !");
-            }
-        } else {
-            console.log("You played an unvalid hand");
-        };
-        console.log(`You ${humanScore} - Computer ${computerScore}`);
-    };
+
     for (let i = 0; i < 5; i++) {
         let computerChoice = getComputerChoice();
         let humanChoice = getHumanChoice();
         playRound(humanChoice, computerChoice);
     }
-};
+};*/
 
-playGame();
+/// DOM manipulation
 
+const playMoveButtons = document.querySelectorAll(".playmove");
+console.log(playMoveButtons);
+playMoveButtons.forEach((playMoveButton) => {
+    playMoveButton.addEventListener("click", () => {
+        console.log(playMoveButton.id);
+        return playMoveButton.id;
+    });
+});
+
+const score_container = document.querySelector(".score_container");
+
+
+/*playGame();
+*/
